2a29ca568c05bfaeb854b47e6c164a50012b159a
xuning <xuning@zbj.com>
2021-10-13 18:04:55 +0800

    提取目前的几个方法

---
+++ src/test/java/com/nk/common/CreateJfreeBarChart2.java
@@ -0,0 +1,222 @@
+package com.nk.common;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
+import org.jfree.chart.ui.TextAnchor;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.awt.*;
+
+public class CreateJfreeBarChart2 {
+    // 设置柱状图上方的标题
+    private static String chartTitle = "环境污染指数分布图";
+
+    // 设置Y轴的标注
+    private static String rangeAxisLabel = "污染指数";
+
+    // 设置X轴的标注
+    private static String domainAxisLabel = "城市";
+
+    // 设置数据集合
+    private static CategoryDataset Dataset = createDataset2();
+    // 设置柱状图的柱子顶部是否显示数据
+    private static boolean baseItemLabelsVisible = true;
+    // 设置生成图片的宽度
+    private static int width = 500;
+    // 设置生成图片的高度
+    private static int height = 800;
+
+    /**
+     * 创建柱状图
+     */
+    public static JFreeChart iCreateBarChart() {
+        // TODO Auto-generated method stub
+        //        iSetBarChart(chart);
+//        WriteChartAsImage.writeChartAsImage(chart, getSaveImgUrlAndName(), getWidth(), getHeight());
+//        return ChartFactory.createBarChart(getChartTitle(), getDomainAxisLabel(), getRangeAxisLabel(), getDataset(), PlotOrientation.VERTICAL, true, false, false);
+        return ChartFactory.createBarChart(getChartTitle(), null, null, getDataset(), PlotOrientation.HORIZONTAL, false, false, false);
+    }
+
+    /**
+     * 设置柱状图的样式
+     *
+     * @param chart
+     */
+    public static CategoryPlot iSetBarChart(JFreeChart chart) {
+        CategoryPlot categoryplot = chart.getCategoryPlot();// 图本身
+//        ValueAxis rangeAxis = categoryplot.getRangeAxis();
+        // 设置Y轴的提示文字样式
+//        rangeAxis.setLabelFont(new Font("微软雅黑", Font.PLAIN, 12));
+        // 设置Y轴刻度线的长度
+//        rangeAxis.setTickMarkInsideLength(10f);
+//        rangeAxis.setTickMarkOutsideLength(10f);
+
+        CategoryAxis domainAxis = categoryplot.getDomainAxis();
+        // 设置X轴下的标签文字
+        domainAxis.setLabelFont(new Font("微软雅黑", Font.PLAIN, 24));
+        // 设置X轴上提示文字样式
+        domainAxis.setTickLabelFont(new Font("微软雅黑", Font.PLAIN, 16));
+
+        // 设置Y轴的数字为百分比样式显示
+//        DecimalFormat df = new DecimalFormat("0.0%");
+        NumberAxis vn = (NumberAxis) categoryplot.getRangeAxis();
+        // 隐藏Y轴数字
+        vn.setVisible(false);
+//        vn.setNumberFormatOverride(df);
+        // 使柱状图反过来显示
+        // vn.setInverted(true);
+        // vn.setVerticalTickLabels(true);
+
+        // 自定义柱状图中柱子的样式
+        BarRenderer barRenderer = (BarRenderer) categoryplot.getRenderer();
+        // 给series Bar 指定配色
+        barRenderer.setSeriesPaint(0, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(1, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(2, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(3, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(4, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(5, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(6, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(7, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(8, Color.decode("#5b9bd5"));
+        barRenderer.setSeriesPaint(9, Color.decode("#5b9bd5"));
+
+        // 设置柱状图的顶端显示数字
+        barRenderer.setIncludeBaseInRange(true);
+        barRenderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+        barRenderer.setDefaultItemLabelsVisible(isBaseItemLabelsVisible());
+        //如果数值没有显示空间，设置显示格式
+        ItemLabelPosition itemLabelPositionFallback = new ItemLabelPosition(
+                ItemLabelAnchor.OUTSIDE12, TextAnchor.BASELINE_CENTER,
+                TextAnchor.HALF_ASCENT_LEFT, 0D);
+        barRenderer.setPositiveItemLabelPositionFallback(itemLabelPositionFallback);
+        barRenderer.setNegativeItemLabelPositionFallback(itemLabelPositionFallback);
+        categoryplot.setRenderer(barRenderer);
+
+        // 设置柱子为平面图不是立体的
+        barRenderer.setBarPainter(new StandardBarPainter());
+        // 设置柱状图之间的距离0.1代表10%；
+        barRenderer.setItemMargin(0.05);
+        // 设置柱子宽度
+        barRenderer.setMaximumBarWidth(0.02);
+        // 设置柱子的阴影，false代表没有阴影
+        barRenderer.setShadowVisible(false);
+
+        // 设置图的背景为白色
+        categoryplot.setBackgroundPaint(Color.WHITE);
+        // 设置背景虚线的颜色
+        categoryplot.setRangeGridlinePaint(Color.decode("#B6A2DE"));
+        // 去掉柱状图的背景边框，使边框不可见
+        categoryplot.setOutlineVisible(false);
+        // 设置标题的字体样式
+        chart.getTitle().setFont(new Font("微软雅黑", Font.PLAIN, 24));
+        // 设置图表下方图例上的字体样式
+//        chart.getLegend().setItemFont(new Font("微软雅黑", Font.PLAIN, 12));
+
+        categoryplot.setRenderer(barRenderer);
+
+        return categoryplot;
+    }
+
+    /**
+     * 数据集合
+     *
+     * @return
+     */
+    public static CategoryDataset createDataset2() {
+
+        DefaultCategoryDataset result = new DefaultCategoryDataset();
+
+        String series1 = "泸州智通自动化设备有限公司";
+        String series2 = "四川长江工程起重机有限责任公司";
+        String series3 = "泸州市驰腾科技有限公司";
+        String series4 = "四川海搏液压机械有限公司";
+        String series5 = "泸州睿大智能变速器有限公司";
+        String series6 = "四川邦立重机有限公司";
+        String series7 = "泸州成邦机械工程有限公司";
+        String series8 = "泸州长江液压密封件有限公司";
+        String series9 = "泸州长江机械有限公司";
+        String series10 = "泸州邦立减速机有限公司";
+        String type1 = "公司";
+        result.addValue(88, type1, series1);
+        result.addValue(79, type1, series2);
+        result.addValue(54, type1, series3);
+        result.addValue(50, type1, series4);
+        result.addValue(44, type1, series5);
+        result.addValue(43, type1, series6);
+        result.addValue(42, type1, series7);
+        result.addValue(42, type1, series8);
+        result.addValue(40, type1, series9);
+        result.addValue(38, type1, series10);
+        return result;
+
+    }
+
+
+    public static String getChartTitle() {
+        return chartTitle;
+    }
+
+    public static void setChartTitle(String chartTitle) {
+        CreateJfreeBarChart2.chartTitle = chartTitle;
+    }
+
+    public static String getRangeAxisLabel() {
+        return rangeAxisLabel;
+    }
+
+    public static void setRangeAxisLabel(String rangeAxisLabel) {
+        CreateJfreeBarChart2.rangeAxisLabel = rangeAxisLabel;
+    }
+
+    public static String getDomainAxisLabel() {
+        return domainAxisLabel;
+    }
+
+    public static void setDomainAxisLabel(String domainAxisLabel) {
+        CreateJfreeBarChart2.domainAxisLabel = domainAxisLabel;
+    }
+
+    public static CategoryDataset getDataset() {
+        return Dataset;
+    }
+
+    public static void setDataset(CategoryDataset dataset) {
+        Dataset = dataset;
+    }
+
+    public static boolean isBaseItemLabelsVisible() {
+        return baseItemLabelsVisible;
+    }
+
+    public static void setBaseItemLabelsVisible(boolean baseItemLabelsVisible) {
+        CreateJfreeBarChart2.baseItemLabelsVisible = baseItemLabelsVisible;
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static void setWidth(int width) {
+        CreateJfreeBarChart2.width = width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
+    public static void setHeight(int height) {
+        CreateJfreeBarChart2.height = height;
+    }
+
+}
--- src/test/java/com/nk/common/PDFTest.java
+++ src/test/java/com/nk/common/PDFTest.java
@@ -16,13 +16,10 @@ import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.title.TextTitle;
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
 
 import java.awt.*;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -31,8 +28,6 @@ import java.text.NumberFormat;
 public class PDFTest {
     private static final String FILE_URL = "C:\\Users\\xn\\Pictures\\";
     private static final int alarmNum = 123;
-
-    ChartUtil chartUtil;
 
     public static void main(String[] args) {
         Document document = new Document();
@@ -57,54 +52,143 @@ public class PDFTest {
 //            img.setAlignment(Image.ALIGN_CENTER);
 //            img.scaleAbsolute(328, 370);
 //            document.add(img);
-            /**
-             * 生成饼状统计图
-             */
-            PieDataset dataset = pieDataSet();
-            JFreeChart chart = ChartFactory.createPieChart(
-                    " 项目进度分布", // chart title
-                    dataset,// data
-                    true,// include legend
-                    true,
-                    false
-            );
-            PiePlot plot = (PiePlot) chart.getPlot();
-            //设置扇区颜色
-            plot.setSectionPaint( " 非高新企业",new Color(222, 235, 247));
-            plot.setSectionPaint( " 高新企业",new Color(91, 155, 213));
-            // 设置扇区的线条颜色
-            plot.setDefaultSectionOutlinePaint(new Color(255, 255, 255));
-            // 设置扇区的线条大小
-            plot.setDefaultSectionOutlineStroke(new BasicStroke(2));
-            //设置Label字体
-            plot.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
-            //设置legend字体
-            chart.getLegend().setItemFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
-            // 图片中显示百分比:默认方式
-            plot.setLabelGenerator(new StandardPieSectionLabelGenerator(StandardPieToolTipGenerator.DEFAULT_TOOLTIP_FORMAT));
-            // 图片中显示百分比:自定义方式，{0} 表示选项， {1} 表示数值， {2} 表示所占比例 ,小数点后两位
-            plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1}({2})", NumberFormat.getNumberInstance(), new DecimalFormat("0.00%")));
-            // 图例显示百分比:自定义方式， {0} 表示选项， {1} 表示数值， {2} 表示所占比例
-            plot.setLegendLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1}({2})"));
-            // 设置背景色为白色
-            chart.setBackgroundPaint(Color.white);
-            // 指定图片的透明度(0.0-1.0)
-            plot.setForegroundAlpha(1.0f);
-            // 指定显示的饼图上圆形(false)还是椭圆形(true)
-            plot.setCircular(true);
-            // 设置图标题的字体
-            java.awt.Font font = new java.awt.Font(" 黑体", java.awt.Font.CENTER_BASELINE, 20);
-            TextTitle title = new TextTitle("饼状图");
-            title.setFont(font);
-            chart.setTitle(title);
-            FileOutputStream fos_jpg = null;
-            try {
-                fos_jpg = new FileOutputStream(FILE_URL + "饼状图.jpg");
-                ChartUtils.writeChartAsJPEG(fos_jpg, 0.7f, chart, 800, 1000, null);
-                fos_jpg.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            //生成饼状统计图
+            pieDataSet(document, fontChinese_content);
+            //柱状图
+            barDataset(document, fontChinese_content);
+            //折线图
+            lineDataset(document, fontChinese_content);
+
+            System.out.println("over");
+            document.close();
+        } catch (DocumentException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 折线图
+     *
+     * @return
+     */
+    private static void lineDataset(Document document, Font fontChinese_content) {
+        DefaultCategoryDataset lineDataset = new DefaultCategoryDataset();
+        //添加第一季度数据
+        lineDataset.addValue(6000, "第一季度", "J2SE类");
+        lineDataset.addValue(3000, "第一季度", "J2ME类");
+        lineDataset.addValue(12000, "第一季度", "J2EE类");
+        //添加第二季度数据
+        lineDataset.addValue(8000, "第二季度", "J2SE类");
+        lineDataset.addValue(4000, "第二季度", "J2ME类");
+        lineDataset.addValue(6000, "第二季度", "J2EE类");
+        //添加第三季度数据
+        lineDataset.addValue(5000, "第三季度", "J2SE类");
+        lineDataset.addValue(4000, "第三季度", "J2ME类");
+        lineDataset.addValue(8000, "第三季度", "J2EE类");
+        //添加第四季度数据
+        lineDataset.addValue(8000, "第四季度", "J2SE类");
+        lineDataset.addValue(2000, "第四季度", "J2ME类");
+        lineDataset.addValue(9000, "第四季度", "J2EE类");
+        JFreeChart lineChart = ChartFactory.createLineChart("java图书销量",
+                "java图书",
+                "销量",
+                lineDataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                true
+        );
+        lineChart.getLegend().setItemFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 16));
+        //获取title
+        lineChart.getTitle().setFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 16));
+
+        //获取绘图区对象
+        CategoryPlot linePlot = lineChart.getCategoryPlot();
+        linePlot.setBackgroundAlpha(0.1f);
+
+        //获取坐标轴对象
+        CategoryAxis lineAxis = linePlot.getDomainAxis();
+        //设置坐标轴字体
+        lineAxis.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
+        //设置坐标轴标尺值字体（x轴）
+        lineAxis.setTickLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
+        //获取数据轴对象（y轴）
+        ValueAxis rangeAxis = linePlot.getRangeAxis();
+        rangeAxis.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
+
+
+        /*
+         * 生成图片
+         */
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(FILE_URL + "折线图.jpg");
+            ChartUtils.writeChartAsJPEG(fos, 0.7f, lineChart, 600, 300);
+
+
+            Paragraph lineParagraph = new Paragraph("03、折线图测试", fontChinese_content);
+            lineParagraph.setAlignment(Paragraph.ALIGN_LEFT);
+            document.add(lineParagraph);
+            Image image = Image.getInstance(FILE_URL + "折线图.jpg");
+            image.setAlignment(Image.ALIGN_CENTER);
+            image.scaleAbsolute(600, 300);
+            document.add(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 饼状图
+     *
+     * @return
+     */
+    private static void pieDataSet(Document document, Font fontChinese_content) {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue(" 高新企业", new Double(83));
+        dataset.setValue(" 非高新企业", new Double(17));
+        JFreeChart chart = ChartFactory.createPieChart(
+                "园区报告", // chart title
+                dataset,// data
+                true,// include legend
+                true,
+                false
+        );
+        PiePlot plot = (PiePlot) chart.getPlot();
+        //设置扇区颜色
+        plot.setSectionPaint(" 非高新企业", new Color(222, 235, 247));
+        plot.setSectionPaint(" 高新企业", new Color(91, 155, 213));
+        // 设置扇区的线条颜色
+        plot.setDefaultSectionOutlinePaint(new Color(255, 255, 255));
+        // 设置扇区的线条大小
+        plot.setDefaultSectionOutlineStroke(new BasicStroke(2));
+        //设置Label字体
+        plot.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
+        //设置legend字体
+        chart.getLegend().setItemFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
+        // 图片中显示百分比:默认方式
+        plot.setLabelGenerator(new StandardPieSectionLabelGenerator(StandardPieToolTipGenerator.DEFAULT_TOOLTIP_FORMAT));
+        // 图片中显示百分比:自定义方式，{0} 表示选项， {1} 表示数值， {2} 表示所占比例 ,小数点后两位
+        plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1}({2})", NumberFormat.getNumberInstance(), new DecimalFormat("0.00%")));
+        // 图例显示百分比:自定义方式， {0} 表示选项， {1} 表示数值， {2} 表示所占比例
+        plot.setLegendLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1}({2})"));
+        // 设置背景色为白色
+        chart.setBackgroundPaint(Color.white);
+        // 指定图片的透明度(0.0-1.0)
+        plot.setForegroundAlpha(1.0f);
+        // 指定显示的饼图上圆形(false)还是椭圆形(true)
+        plot.setCircular(true);
+        // 设置图标题的字体
+        java.awt.Font font = new java.awt.Font(" 黑体", java.awt.Font.CENTER_BASELINE, 20);
+        TextTitle title = new TextTitle("饼状图");
+        title.setFont(font);
+        chart.setTitle(title);
+        FileOutputStream fos_jpg = null;
+        try {
+            fos_jpg = new FileOutputStream(FILE_URL + "饼状图.jpg");
+            ChartUtils.writeChartAsJPEG(fos_jpg, 0.7f, chart, 800, 1000, null);
+            fos_jpg.close();
+
 
             document.newPage();
             Paragraph pieParagraph = new Paragraph("02、饼状图测试", fontChinese_content);
@@ -114,28 +198,25 @@ public class PDFTest {
             pieImage.setAlignment(Image.ALIGN_CENTER);
             pieImage.scaleAbsolute(328, 370);
             document.add(pieImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static void barDataset(Document document, Font fontChinese_content) {
+        JFreeChart jFreeChart = CreateJfreeBarChart.iCreateBarChart();
+        CreateJfreeBarChart.iSetBarChart(jFreeChart);
 
-            /**
-             * 柱状图
-             */
-            JFreeChart jFreeChart = CreateJfreeBarChart.iCreateBarChart();
-            CreateJfreeBarChart.iSetBarChart(jFreeChart);
-
-            // 设置图标题的字体
-            java.awt.Font font2 = new java.awt.Font(" 黑体", java.awt.Font.CENTER_BASELINE, 20);
-            TextTitle title2 = new TextTitle("柱状图");
-            title2.setFont(font2);
-            jFreeChart.setTitle(title2);
-            FileOutputStream fos_jpg2 = null;
-            try {
-                fos_jpg2 = new FileOutputStream(FILE_URL + "企业专利申请量.jpg");
-                ChartUtils.writeChartAsJPEG(fos_jpg2, 0.7f, jFreeChart, 800, 600, null);
-                fos_jpg2.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+        // 设置图标题的字体
+        java.awt.Font font2 = new java.awt.Font(" 黑体", java.awt.Font.CENTER_BASELINE, 20);
+        TextTitle title2 = new TextTitle("柱状图");
+        title2.setFont(font2);
+        jFreeChart.setTitle(title2);
+        FileOutputStream fos_jpg2 = null;
+        try {
+            fos_jpg2 = new FileOutputStream(FILE_URL + "企业专利申请量.jpg");
+            ChartUtils.writeChartAsJPEG(fos_jpg2, 0.7f, jFreeChart, 800, 600, null);
+            fos_jpg2.close();
             document.newPage();
             Paragraph barParagraph = new Paragraph("六、企业专利申请量top10", fontChinese_content);
             barParagraph.setAlignment(Paragraph.ALIGN_LEFT);
@@ -144,98 +225,8 @@ public class PDFTest {
             barImage.setAlignment(Image.ALIGN_CENTER);
             barImage.scaleAbsolute(800, 600);
             document.add(barImage);
-
-
-            /**
-             * 折线图
-             */
-            CategoryDataset lineDataset = lineDataset();
-            JFreeChart lineChart = ChartFactory.createLineChart("java图书销量",
-                    "java图书",
-                    "销量",
-                    lineDataset,
-                    PlotOrientation.VERTICAL,
-                    true,
-                    true,
-                    true
-            );
-            lineChart.getLegend().setItemFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 16));
-            //获取title
-            lineChart.getTitle().setFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 16));
-
-            //获取绘图区对象
-            CategoryPlot linePlot = lineChart.getCategoryPlot();
-            linePlot.setBackgroundAlpha(0.1f);
-
-            //获取坐标轴对象
-            CategoryAxis lineAxis = linePlot.getDomainAxis();
-            //设置坐标轴字体
-            lineAxis.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
-            //设置坐标轴标尺值字体（x轴）
-            lineAxis.setTickLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
-            //获取数据轴对象（y轴）
-            ValueAxis rangeAxis = linePlot.getRangeAxis();
-            rangeAxis.setLabelFont(new java.awt.Font("微软雅黑", java.awt.Font.BOLD, 12));
-
-
-            /*
-             * 生成图片
-             */
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(FILE_URL + "折线图.jpg");
-                ChartUtils.writeChartAsJPEG(fos, 0.7f, lineChart, 600, 300);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            Paragraph lineParagraph = new Paragraph("03、折线图测试", fontChinese_content);
-            lineParagraph.setAlignment(Paragraph.ALIGN_LEFT);
-            document.add(lineParagraph);
-            Image image = Image.getInstance(FILE_URL + "折线图.jpg");
-            image.setAlignment(Image.ALIGN_CENTER);
-            image.scaleAbsolute(600, 300);
-            document.add(image);
-
-            System.out.println("over");
-            document.close();
-        } catch (FileNotFoundException | DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static CategoryDataset lineDataset() {
-        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        //添加第一季度数据
-        dataSet.addValue(6000, "第一季度", "J2SE类");
-        dataSet.addValue(3000, "第一季度", "J2ME类");
-        dataSet.addValue(12000, "第一季度", "J2EE类");
-        //添加第二季度数据
-        dataSet.addValue(8000, "第二季度", "J2SE类");
-        dataSet.addValue(4000, "第二季度", "J2ME类");
-        dataSet.addValue(6000, "第二季度", "J2EE类");
-        //添加第三季度数据
-        dataSet.addValue(5000, "第三季度", "J2SE类");
-        dataSet.addValue(4000, "第三季度", "J2ME类");
-        dataSet.addValue(8000, "第三季度", "J2EE类");
-        //添加第四季度数据
-        dataSet.addValue(8000, "第四季度", "J2SE类");
-        dataSet.addValue(2000, "第四季度", "J2ME类");
-        dataSet.addValue(9000, "第四季度", "J2EE类");
-        return dataSet;
-    }
-
-    private static PieDataset pieDataSet() {
-        DefaultPieDataset dataset = new DefaultPieDataset();
-//        dataset.setValue(" 市场前期", new Double(10));
-//        dataset.setValue(" 立项", new Double(15));
-//        dataset.setValue(" 计划", new Double(10));
-//        dataset.setValue(" 需求与设计", new Double(10));
-//        dataset.setValue(" 执行控制", new Double(35));
-        dataset.setValue(" 非高新企业", new Double(17));
-        dataset.setValue(" 高新企业", new Double(83));
-        return dataset;
     }
 }
